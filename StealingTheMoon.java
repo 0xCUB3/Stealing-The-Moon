fb94baf42926a8d474bc6334285cbca377bc1814
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2024-11-07 14:32:25 -0500

    Steal the moon.

---
+++ StealingTheMoon.java
@@ -0,0 +1,7 @@
+import java.lang.System;
+
+public class StealingTheMoon {
+    public static void main(String[] args) {
+        System.out.println("We're not actually stealing the moon. Our lawyers advised us against it.");
+    }
+}
